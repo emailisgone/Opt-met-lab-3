27c48c1adfdf25f1a12e85a0e004d4727188a0bd
emailisgone <nikita.exe0@gmail.com>
2024-11-28 17:35:41 +0200

    Small optimizations

--- optfunc.py
+++ optfunc.py
@@ -9,10 +9,10 @@ class Optimization:
         return -(x[0]*x[1]*x[2])
     
     def g(self, x):
-        return 2*(x[0]*x[1]+x[0]*x[2]+x[1]*x[2])-1   # 2ab+2ac+2bc=1  =>  2(ab+ac+bc)-1=0  =>  gi(a,b,c)=0
+        return 2*(x[0]*x[1]+x[0]*x[2]+x[1]*x[2])-1   # 2ab+2ac+2bc=1  =>  2(ab+ac+bc)-1=0  =>  gi(a,b,c)=0  - lygibinis apribojimas
     
     def h(self, x): 
-        return [-x[0], -x[1], -x[2]]                 # a,b,c>0  => hi(a,b,c)>=0
+        return [-x[0], -x[1], -x[2]]                 # a,b,c>0  => hi(a,b,c)>=0     - nelygibinis apribojimas
     
     def reset(self):
         Optimization.counter = 0
