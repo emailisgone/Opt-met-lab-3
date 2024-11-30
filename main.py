285f76d31c3b3e03b14f933c2f6e195b862305a0
emailisgone <nikita.exe0@gmail.com>
2024-11-30 15:51:27 +0200

    Finished. Final version for the time being

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-'''
-Kokia turėtų būti stačiakampio gretasienio formos dėžė, kad vienetiniam paviršiaus plotui jos tūris būtų maksimalus?
-
-1. Aprašykite tikslo funkciją f(X), lygybinio ir nelygybinių apribojimų funkcijas gi(X) ir hi(X) taip, kad optimizavimo uždavinys būtų formuluojamas min f(X), gi(X)=0, hi(X)≤0.
-2. Apskaičiuokite funkcijų f(X), gi(X), hi(X) reikšmes taškuose X0=(0,0,0), X1=(1,1,1) ir Xm=(1/10,5/10,7/10).
-3. Aprašykite kvadratinę baudos funkciją, apimančią tikslo funkciją ir apribojimus.
-4. Patyrinėkite baudos daugiklio įtaką baudos funkcijos reikšmėms.
-5. Minimizuokite baudos funkciją praeitame laboratoriniame darbe sukurtu optimizavimo be apribojimų algoritmu sprendžiant optimizavimo uždavinių seką su mažėjančia parametro r seka, kai pirmasis sekos uždavinys optimizuojamas pradedant iš taškų X_0, X_1 ir X_m, o kiekvieno paskesnio uždavinio pradinis taškas yra ankstesnio uždavinio sprendinys. 
-6. Palyginkite rezultatus: gauti sprendiniai, rastas funkcijos minimumo įvertis, atliktų žingsnių ir funkcijų skaičiavimų skaičius priklausomai nuo pradinio taško.
-'''
 from imports import datetime, math, np, sp, plt, scp
 from optfunc import Optimization
 from penalty import minPen
